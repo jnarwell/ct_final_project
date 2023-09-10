9d5a3f51ed1b2cb24b6ba386c5d2f8fc3ab10d05
jnarwell <jcmarwell@gmail.com>
2023-09-09 19:04:16 -0700

    finished tes

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,7 @@ from flask import render_template, redirect, url_for
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/ponder')
+def ponder():
+    return render_template('ponder.html')
